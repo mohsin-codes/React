678589eed06d2808f520004d385e39655ebdfc2d
Mohsin Khan <mohsink9899@gmail.com>
2025-02-23 13:25:32 +0530

    Learned about Override, async and using setTimeout function on Components

--- Components/Functional Components/Functional Components in React/Functional Components in React/src/App.jsx
+++ Components/Functional Components/Functional Components in React/Functional Components in React/src/App.jsx
@@ -2,35 +2,113 @@ import './App.css'
 import React, { useState} from 'react'
 
 const App = () => {
-  const [greeting, setGreeting] = useState(
-    'Hello Function Component!'
-  );
-  
-  const handleChange = (event) => setGreeting(event.target.value);
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    setTimeout(
+      () => setCount(currentCount => currentCount + 1)
+    , 1000)
+  }
+
+  const handleDecrement = () => {
+    setTimeout(
+      () => setCount(currentCount => currentCount - 1)
+    , 1000)
+  }
 
   return (
-    <>
-      <Headline headline={greeting}/>
-      <Input value={greeting} onChangeInput={handleChange}>
-        Set Greetings: 
-      </Input>
-    </>
+    <div>
+      <h1>{count}</h1>
+
+      <Button handleClick={handleIncrement}>Increment</Button>
+      <Button handleClick={handleDecrement}>Decrement</Button>
+    </div>
   )
 }
 
-const Headline = ({headline}) => <h1>{headline}</h1>;
-
-const Input = ({value, onChangeInput, children}) =>(
-  <label>
-    {children} 
-    <input 
-          type="text"
-          value={value}
-          onChange={onChangeInput} />
-  </label>
+const Button = ({handleClick, children}) => (
+  <button
+    type='button'
+    onClick={handleClick}>  
+      {children}
+  </button>
 )
 
 export default App;
+
+//Using async in Component function
+// const App = () => {
+//   const sayHello = () => {
+//     setTimeout(() => console.log("Hello"),1000);
+//   }
+
+//   return <Button handleClick={sayHello} />
+// }
+
+// const Button = ({handleClick}) => (
+//   <button 
+//     type='button'
+//     onClick={handleClick}>
+//     Button
+//   </button>
+// )
+
+// export default App;
+
+//Override the component function
+// const App = () => {
+//   const sayHello = () => console.log("Hello");
+
+//   return <Button handleClick={sayHello} />
+// };
+
+// const Button = ({handleClick}) => {
+//   const sayDefault = () => console.log("Default");
+
+//   const onClick = handleClick || sayDefault;
+
+//   return (
+//     <button 
+//       type='button'
+//       onClick={onClick}>
+//         Button
+//       </button>
+//   );
+// };
+
+// export default App;
+
+//using EventHandler and Callback Function
+// const App = () => {
+//   const [greeting, setGreeting] = useState(
+//     'Hello Function Component!'
+//   );
+  
+//   const handleChange = (event) => setGreeting(event.target.value);
+
+//   return (
+//     <>
+//       <Headline headline={greeting}/>
+//       <Input value={greeting} onChangeInput={handleChange}>
+//         Set Greetings: 
+//       </Input>
+//     </>
+//   )
+// }
+
+// const Headline = ({headline}) => <h1>{headline}</h1>;
+
+// const Input = ({value, onChangeInput, children}) =>(
+//   <label>
+//     {children} 
+//     <input 
+//           type="text"
+//           value={value}
+//           onChange={onChangeInput} />
+//   </label>
+// )
+
+// export default App;
 
 
 
