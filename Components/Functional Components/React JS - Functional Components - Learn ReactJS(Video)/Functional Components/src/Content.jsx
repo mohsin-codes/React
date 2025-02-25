4dd91b6443fa7001faee62af96b1cbe199ff6cf3
Mohsin Khan <mohsink9899@gmail.com>
2025-02-26 01:52:55 +0530

    Functional Components completed!

---
+++ Components/Functional Components/React JS - Functional Components - Learn ReactJS(Video)/Functional Components/src/Content.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Content = () => {
+  return (
+    <main>
+        <img src="assets/react.svg" alt="" />
+        <p>
+            Hello Mohsin!
+        </p>
+    </main>
+  )
+}
+
+export default Content
