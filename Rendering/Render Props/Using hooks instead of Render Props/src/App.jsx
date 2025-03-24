48b9f555b36fe1054783f9cb27a17e40165dbcf2
Mohsin Khan <mohsink9899@gmail.com>
2025-03-25 00:42:39 +0530

    Using hooks instead of render props

--- Rendering/Render Props/Using hooks instead of Render Props/src/App.jsx
+++ Rendering/Render Props/Using hooks instead of Render Props/src/App.jsx
@@ -1,33 +1,27 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const useMousePosition = () => {
+  const [position, setPosition] = useState({x: 0, y: 0});
 
+  useEffect(() => {
+    const updatePostion = (event) => {
+      setPosition({x: event.clientX, y: event.clientY});
+    }
+    window.addEventListener("mousemove", updatePostion);
+    return () => window.removeEventListener("mousemove", updatePostion);
+  }, []);
+
+  return position;
+}
+
+function App() {
+  const {x, y} = useMousePosition();
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2>Mouse Position : {x}, {y}</h2>
     </>
   )
 }
